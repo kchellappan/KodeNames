aa1de95e60684c6f303a9a404b4ac1a129079be1
Karthik Chellappan <k.chellappan92@gmail.com>
2024-09-20 23:48:01 -0700

    Parsed top 250 tamil movies off IMdB and adding this as an option for personal use.

--- scripts/index.js
+++ scripts/index.js
@@ -38,6 +38,9 @@ function fire() {
 
 	var option = $('#gameMode :selected').val();
 	switch (option) {
+		case 'tamil':
+			sessionData = tamilmoviesdata.slice(0);
+			break;
 		case 'spanish':
 			sessionData = spanishData.slice(0);
 			break;
